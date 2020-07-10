143291be82d73ab9b8616746cc417a409a1a253f
Mubashir Hanif <mubashirhanif93@gmail.com>
2020-07-10 21:06:30 +0200

    reformatted code, introduced new components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Error from './components/Error/Error';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={ Home } exact />
+          <Route component={ Error } />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Error/Error.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Error.css';
+
+class Error extends Component {
+  render() {
+    return (
+      <div className="Error">
+        <h1>Oops!!</h1>
+      </div>
+    );
+  }
+}
+
+export default Error;
